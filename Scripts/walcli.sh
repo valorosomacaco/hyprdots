9dbeef41e06bf858627178e828a2fc7f8accd183
valorosomacaco <valorosomacaco@gmail.com>
2026-08-31 02:37:08 +0200

    FixScript

--- Scripts/walcli.sh
+++ Scripts/walcli.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-	echo "Usage: $= <path_to_image>"
-	exit 1
-fi
 
 IMAGE="$1"
 
 
 awww img "$IMAGE" --transition-type="random" --transition-fps="60"
-
+pkill -f /usr/lib/xdg-desktop-portal-gtk
 matugen --prefer darkness --show-colors -t scheme-tonal-spot image "$IMAGE"
-
-pkill -f polkit-gnome-authe && /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1; pkill swayosd-server && swayosd-server; systemctl --user stop xdg-desktop-portal-gtk && systemctl --user start xdg-desktop-portal-gtk
-
-getAdwaitaScheme="$(gsettings get org.gnome.desktop.interface color-scheme)"
-if [ "$getAdwaitaScheme" = "'prefer-dark'" ]; then
-  gsettings set org.gnome.desktop.interface color-scheme prefer-light && gsettings set org.gnome.desktop.interface color-scheme prefer-dark;
-else
-  gsettings set org.gnome.desktop.interface color-scheme prefer-dark && gsettings set org.gnome.desktop.interface color-scheme prefer-light;
-fi
+pkill swayosd-server 
+swayosd-server &
+disown
+pkill -f polkit-gnome-authe && /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 
