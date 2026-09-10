5f26523d0983b0420c4d87a30314bb1c513c3242
valorosomacaco <valorosomacaco@gmail.com>
2026-09-10 18:47:31 +0200

    NoctaliaConfig

---
+++ hypr/colors.lua
@@ -0,0 +1,104 @@
+return {
+    image = "/home/ale/Pictures/Wallpapers/a_sunset_over_a_body_of_water_with_Marfa_lights_in_the_background.png",
+
+    background = "0xff171216",
+
+    error = "0xffffb4ab",
+
+    error_container = "0xff93000a",
+
+    inverse_on_surface = "0xff352f34",
+
+    inverse_primary = "0xff7c4e7e",
+
+    inverse_surface = "0xffebdfe6",
+
+    on_background = "0xffebdfe6",
+
+    on_error = "0xff690005",
+
+    on_error_container = "0xffffdad6",
+
+    on_primary = "0xff49204d",
+
+    on_primary_container = "0xffffd6fc",
+
+    on_primary_fixed = "0xff320936",
+
+    on_primary_fixed_variant = "0xff623765",
+
+    on_secondary = "0xff3c2b3b",
+
+    on_secondary_container = "0xfff5dbf1",
+
+    on_secondary_fixed = "0xff251626",
+
+    on_secondary_fixed_variant = "0xff534152",
+
+    on_surface = "0xffebdfe6",
+
+    on_surface_variant = "0xffd0c3cc",
+
+    on_tertiary = "0xff4c251e",
+
+    on_tertiary_container = "0xffffdad3",
+
+    on_tertiary_fixed = "0xff33110b",
+
+    on_tertiary_fixed_variant = "0xff673b33",
+
+    outline = "0xff998d96",
+
+    outline_variant = "0xff4d444c",
+
+    primary = "0xffedb4eb",
+
+    primary_container = "0xff623765",
+
+    primary_fixed = "0xffffd6fc",
+
+    primary_fixed_dim = "0xffedb4eb",
+
+    scrim = "0xff000000",
+
+    secondary = "0xffd8bfd4",
+
+    secondary_container = "0xff534152",
+
+    secondary_fixed = "0xfff5dbf1",
+
+    secondary_fixed_dim = "0xffd8bfd4",
+
+    shadow = "0xff000000",
+
+    source_color = "0xff3d323c",
+
+    surface = "0xff171216",
+
+    surface_bright = "0xff3e373c",
+
+    surface_container = "0xff241e23",
+
+    surface_container_high = "0xff2e282d",
+
+    surface_container_highest = "0xff393338",
+
+    surface_container_low = "0xff1f1a1f",
+
+    surface_container_lowest = "0xff120d11",
+
+    surface_dim = "0xff171216",
+
+    surface_tint = "0xffedb4eb",
+
+    surface_variant = "0xff4d444c",
+
+    tertiary = "0xfff6b8ac",
+
+    tertiary_container = "0xff673b33",
+
+    tertiary_fixed = "0xffffdad3",
+
+    tertiary_fixed_dim = "0xfff6b8ac",
+
+}
